43994479c1047ca677109fc1179d7180af257236
Tran Phuc Vinh <vinhtranphuc@gmail.com>
2019-06-28 16:57:42 +0700

    test

--- src/components/FrameComponent.jsx
+++ src/components/FrameComponent.jsx
@@ -7,7 +7,7 @@ class FrameComponent extends Component {
             <>
                 <div className="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
                    <div className = "m-grid m-grid--hor m-grid--root m-page">
-                        
+                        Con chim to
                    </div>
                 </div>
             </>
